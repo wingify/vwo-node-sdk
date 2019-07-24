b141e5e53c05b315cc6ed2b4ee84cb3373e73083
softvar <varun2902@gmail.com>
2019-07-24 18:24:50 +0530

    test(SettingsFile.spec.js): add test cases

--- test/utils/SettingsFileUtil.spec.js
+++ test/utils/SettingsFileUtil.spec.js
@@ -1,5 +1,30 @@
-require('../../lib/utils/SettingsFileUtil');
+const SettingsFileUtil = require('../../lib/utils/SettingsFileUtil');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(3).toBe(3);
+let accountId;
+let sdkKey;
+beforeEach(() => {
+  accountId = 12345;
+  sdkKey = 'aa11110pp22222aa333ff4c5b66r77ee';
 });
+
+describe('SettingsFileUtil', () => {
+  describe('method: get', () => {
+    it('should return undefined if no paramter is passed', () => {
+      expect(SettingsFileUtil.get()).toBeUndefined();
+    });
+
+    it('should return undefined if no sdkKey is passed', () => {
+      expect(SettingsFileUtil.get(accountId)).toBeUndefined();
+    });
+
+    it('should return undefined if no accountId is passed', () => {
+      expect(SettingsFileUtil.get(undefined, sdkKey)).toBeUndefined();
+    });
+
+    it('should return a promise if parameters passed are correct', () => {
+      const settingsFilePromise = SettingsFileUtil.get(accountId, sdkKey);
+
+      expect(typeof settingsFilePromise).toBe('object');
+    });
+  });
+})
