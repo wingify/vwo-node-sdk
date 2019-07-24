a243c4065160ac2056fc8f1f23fb7fdb7fa5e0c1
softvar <varun2902@gmail.com>
2019-07-24 14:45:13 +0530

    test(FunctionUtil.spec): add test cases

--- test/utils/FunctionUtil.spec.js
+++ test/utils/FunctionUtil.spec.js
@@ -1,5 +1,37 @@
-require('../../lib/utils/FunctionUtil');
+const FunctionUtil = require('../../lib/utils/FunctionUtil');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(3).toBe(3);
+describe('DataTypeUtil', () => {
+  describe('method: cloneObject', () => {
+    it('should clone an object', () => {
+      const obj = {
+        a: 1,
+        b: 2
+      }
+      const newObj = FunctionUtil.cloneObject(obj);
+
+      // change obj
+      obj.a = 3;
+      obj.b = 5;
+
+      expect(obj.a).toBe(3);
+      expect(obj.b).toBe(5);
+
+      expect(newObj.a).toBe(1);
+      expect(newObj.b).toBe(2);
+    });
+  });
+
+  describe('method: getRandomNumber', () => {
+    it('should return a number', () => {
+      expect(FunctionUtil.getRandomNumber()).toBeDefined();
+      expect(typeof FunctionUtil.getRandomNumber()).toBe('number');
+    });
+  });
+
+  describe('method: getCurrentUnixTimestamp', () => {
+    it('should return current time in unix format', () => {
+      expect(FunctionUtil.getCurrentUnixTimestamp()).toBeDefined();
+      expect(typeof FunctionUtil.getCurrentUnixTimestamp()).toBe('number');
+    });
+  });
 });
