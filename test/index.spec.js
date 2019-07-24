b9a21ff5335aa33c62f88fdaf6bea01f2c5c4dc2
softvar <varun2902@gmail.com>
2019-07-24 13:11:17 +0530

    test(index.spec.js): add test cases for createInstance method

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,5 +1,63 @@
-require('../lib/');
+const indexFile = require('../lib/');
+const logging = require('../lib/logging');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(3).toBe(3);
+describe('index file', () => {
+  test('should export required modules and APIs', () => {
+    expect(indexFile.logging).toBeDefined();
+    expect(indexFile.setLogger).toBeDefined();
+    expect(indexFile.setLogLevel).toBeDefined();
+    expect(indexFile.getSettingsFile).toBeDefined();
+    expect(indexFile.createInstance).toBeDefined();
+  });
+
+  describe('method: createInstance', () => {
+    test('set log color mode if development mode is passed', () => {
+      const spyOnLoggingSetColor = jest.spyOn(logging, 'setLogColorMode');
+
+      indexFile.createInstance({
+        isDevelopmentMode: true
+      });
+      expect(spyOnLoggingSetColor).toHaveBeenCalledWith(true);
+    });
+
+    test('set log handler and log level if passed by end-user are in desired format', () => {
+      const spyOnLoggingSetHandler = jest.spyOn(logging, 'setLogHandler');
+      const spyOnLoggingSetLevel = jest.spyOn(logging, 'setLogLevel');
+      const loggingConfig = {
+        logger: {
+          log: jest.fn()
+        },
+        level: logging.LogLevelEnum.DEBUG
+      };
+
+      indexFile.createInstance({
+        logging: loggingConfig
+      });
+
+      expect(spyOnLoggingSetHandler).toHaveBeenCalledWith(loggingConfig.logger);
+      expect(spyOnLoggingSetLevel).toHaveBeenCalledWith(logging.LogLevelEnum.NOTSET);
+    });
+
+    test('set log level to user defined log-level', () => {
+      const spyOnLoggingSetLevel = jest.spyOn(logging, 'setLogLevel');
+      const loggingConfig = {
+        level: logging.LogLevelEnum.DEBUG
+      };
+
+      indexFile.createInstance({
+        logging: loggingConfig
+      });
+
+      expect(spyOnLoggingSetLevel).toHaveBeenCalledWith(loggingConfig.level);
+    });
+
+    test('should return a new Object of VWO SDK', () => {
+      const vwoClientinstance = indexFile.createInstance({});
+
+      expect(vwoClientinstance).toBeDefined();
+      expect(vwoClientinstance.activate).toBeDefined();
+      expect(vwoClientinstance.getVariation).toBeDefined();
+      expect(vwoClientinstance.track).toBeDefined();
+    });
+  });
 });
