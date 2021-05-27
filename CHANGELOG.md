778dc172a41b86ae5d14d24352c902bdadf222ce
sanyamjain65 <sanyam.j65@gmail.com>
2021-05-27 19:06:41 +0530

    fix(campaign-name): added camapign name in integrations callback

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+
+## [1.17.1] - 2021-05-27
+
+## Changed
+
+- `campaignName` will be available in integrations callback, if callback is defined.
+
 ## [1.17.0] - 2021-05-18
 
 ## Added
--- dist/vwo-javascript-sdk.js
+++ dist/vwo-javascript-sdk.js
@@ -1,5 +1,5 @@
 /*!
- * vwo-javascript-sdk - v1.16.0
+ * vwo-javascript-sdk - v1.17.0
  * URL - https://github.com/wingify/vwo-node-sdk
  * 
  * Copyright 2019-2021 Wingify Software Pvt. Ltd.
@@ -1495,7 +1495,7 @@ var packageFile = {}; // For javascript-sdk, to keep the build size low
 if (true) {
   packageFile = {
     name: "vwo-javascript-sdk",
-    version: "1.16.0"
+    version: "1.17.0"
   };
 } else {}
 
@@ -4128,6 +4128,7 @@ var DecisionUtil = {
       campaignId: campaign.id,
       campaignKey: campaignKey,
       campaignType: campaign.type,
+      campaignName: campaign.name,
       // campaign segmentation conditions
       customVariables: customVariables,
       // event name
